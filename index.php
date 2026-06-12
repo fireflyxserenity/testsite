8df2b377a149c4c8de5b73d28677dce8cc9e4b1b
fireflyxserenity <firefly.melodie@gmail.com>
2026-06-11 20:04:15 -0400

    Card: remove front hole, add paper grain texture + vignette, restore Hauling

--- index.php
+++ index.php
@@ -13,7 +13,7 @@
     <div class="rule"></div>
     <div class="grid">
       <div class="plank"><div class="icon">📦</div><h3>Moving Aid</h3><p>Heavy lifting, truck loading, full apartment &amp; house moves.</p></div>
-      <div class="plank"><div class="icon">🚛</div><h3>Dump Runs</h3><p>Junk hauled away, garage &amp; basement cleanouts, dump trips.</p></div>
+      <div class="plank"><div class="icon">🚛</div><h3>Hauling</h3><p>Dump runs, junk removal, garage &amp; basement cleanouts.</p></div>
       <div class="plank"><div class="icon">🌾</div><h3>Yard Work</h3><p>Mowing, raking, hedges, gutters &amp; seasonal cleanups.</p></div>
       <div class="plank"><div class="icon">🔨</div><h3>Odd Jobs</h3><p>Fixing stuff, assembly, mounting &amp; anything in between.</p></div>
     </div>
